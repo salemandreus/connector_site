4084dc1b83d88a3c2016eb281dcad61103db0809
salemandreus <salemandreus@gmail.com>
2018-10-11 12:27:08 +0200

    disabled bookings numbers showing

--- meetups_list/views.py
+++ meetups_list/views.py
@@ -6,15 +6,15 @@ def index(request):
     """View function for home page of site."""
 
     num_meetups = Meetup.objects.all().count()
-    num_spaces = BookingSpaces.objects.all().count()
-    num_spaces_available = BookingSpaces.objects.filter(status__exact='u').count()
+    #num_spaces = BookingSpaces.objects.all().count()
+    #num_spaces_available = BookingSpaces.objects.filter(status__exact='u').count()
     
     num_organisers = Organiser.objects.count()
     
     context = {
         'num_meetups': num_meetups,
-        'num_spaces': num_spaces,
-        'num_spaces_available': num_spaces_available,
+        #'num_spaces': num_spaces,
+        #'num_spaces_available': num_spaces_available,
         'num_organisers': num_organisers,
     }
 
